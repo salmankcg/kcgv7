2c2ff33d4484d0acd34e6751a32e8873e1ff9c89
Ashaduzzaman <mukul.ashad@gmail.com>
2021-12-15 18:56:04 +0600

    Complete buzz filter jquery issue

--- public/js/main.js
+++ public/js/main.js
@@ -105185,87 +105185,80 @@ var $filter = jquery__WEBPACK_IMPORTED_MODULE_0___default()('.filter'); // -----
 // ------------------ INIT ----------------- \\\
 // ----------------------------------------- \\\
 
-function init() {
-  var elem = document.querySelector('.press-list');
-
-  var _dataAjax = $filter.data('ajax');
-
-  var msnry = null;
-  var _toggleClick = false;
-  ;
-
-  if (jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).width() > 550) {
-    setTimeout(function () {
-      msnry = new masonry_layout__WEBPACK_IMPORTED_MODULE_1___default.a(elem, {
-        itemSelector: '.item'
-      });
-      msnry.layout();
-      msnry.on('layoutComplete', function () {
-        window.dispatchEvent(new Event('resize'));
-        console.log('update resize');
-      });
-    }, 500);
-  }
-
-  console.log('init 2');
-  $filter.find('.item').on('click', function (e) {
-    e.preventDefault();
-
-    if (!_toggleClick) {
-      $filter.find('.item').removeClass('active');
-      jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).addClass('active');
-      var catID = jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).data('id');
-      var limit = jquery__WEBPACK_IMPORTED_MODULE_0___default()(this).data('limit');
-      jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').addClass('loading');
-      jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').append('<div class="ajax-loader"><svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve"><path fill="#000" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"><animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform></path></svg></div>');
-      jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').find('.message').remove();
-      setTimeout(function () {
-        msnry.destroy(); // msnry = null;
-      }, 1000);
-      jquery__WEBPACK_IMPORTED_MODULE_0___default.a.ajax({
-        url: _dataAjax,
-        type: 'POST',
-        data: {
-          action: 'kcg_filter_buzz_by_category',
-          nonce: object_kcg.nonce,
-          'cat-id': catID
-        }
-      }).done(function (response) {
-        var _html = response['html'];
-        setTimeout(function () {
-          jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').find('.item').remove();
-
-          if (_html == '') {
-            jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').append('<div class="message">Sorry, we don\'t have any item yet.</div>');
-          } else {
-            jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').append(_html);
-          }
-
-          setTimeout(function () {
-            if (jquery__WEBPACK_IMPORTED_MODULE_0___default()(window).width() > 550) {
-              msnry = new masonry_layout__WEBPACK_IMPORTED_MODULE_1___default.a(elem, {
-                itemSelector: '.item'
-              });
-              msnry.layout();
-              msnry.on('layoutComplete', function () {
-                window.dispatchEvent(new Event('resize'));
-                console.log('update resize');
-              });
-            }
-          }, 1000);
-          setTimeout(function () {
-            // window.dispatchEvent(new Event('resize'));
-            jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').find('.ajax-loader').remove();
-            jquery__WEBPACK_IMPORTED_MODULE_0___default()('.press-list').removeClass('loading');
-            _toggleClick = false;
-          }, 1000);
-        }, 1000); // console.log('ajax response '+ response);
-      }).fail(function (response) {
-        console.log(response);
-      });
-      _toggleClick = true;
-    }
-  });
+function init() {// var elem        = document.querySelector('.press-list');
+  // var _dataAjax   = $filter.data('ajax');
+  // var msnry       = null;
+  //  var _toggleClick    = false;;
+  // if($(window).width() > 550){
+  //     setTimeout(function(){
+  //         msnry = new Masonry( elem, {
+  //             itemSelector: '.item',
+  //         });
+  //         msnry.layout();
+  //         msnry.on( 'layoutComplete', function(){
+  //             window.dispatchEvent(new Event('resize'));
+  //             console.log('update resize');
+  //         } );
+  //     }, 500 );
+  // }
+  // console.log('init 2');
+  // $filter.find('.item').on('click', function(e){
+  //     e.preventDefault();
+  //     if(!_toggleClick){
+  //         $filter.find('.item').removeClass('active');
+  //         $(this).addClass('active');
+  //         var catID = $(this).data('id');
+  //         var limit = $(this).data('limit');
+  //         $('.press-list').addClass('loading');
+  //         $('.press-list').append('<div class="ajax-loader"><svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve"><path fill="#000" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"><animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform></path></svg></div>');
+  //         $('.press-list').find('.message').remove();
+  //         setTimeout(function(){
+  //             msnry.destroy();
+  //             // msnry = null;
+  //         },1000);
+  //         $.ajax({
+  //             url: _dataAjax,
+  //             type: 'POST',
+  //             data: {
+  //                 action: 'kcg_filter_buzz_by_category',
+  //                 nonce: object_kcg.nonce,
+  //                 'cat-id': catID,
+  //             },
+  //         }).done(function(response) {
+  //             var _html = response['html'];
+  //             setTimeout(function(){
+  //                 $('.press-list').find('.item').remove();
+  //                 if(_html == ''){
+  //                     $('.press-list').append('<div class="message">Sorry, we don\'t have any item yet.</div>');
+  //                 }else{
+  //                     $('.press-list').append(_html);
+  //                 }
+  //                 setTimeout(function(){
+  //                     if($(window).width() > 550){
+  //                         msnry = new Masonry( elem, {
+  //                             itemSelector: '.item',
+  //                         });
+  //                         msnry.layout();
+  //                         msnry.on( 'layoutComplete', function(){
+  //                             window.dispatchEvent(new Event('resize'));
+  //                             console.log('update resize');
+  //                         } );
+  //                     }
+  //                 }, 1000);
+  //                 setTimeout(function(){
+  //                     // window.dispatchEvent(new Event('resize'));
+  //                     $('.press-list').find('.ajax-loader').remove();
+  //                     $('.press-list').removeClass('loading');
+  //                     _toggleClick = false;
+  //                 }, 1000);
+  //             },1000);
+  //             // console.log('ajax response '+ response);
+  //         }).fail(function(response) {
+  //             console.log(response);
+  //         });
+  //         _toggleClick = true;
+  //     }
+  // });
 } // ----------------------------------------- \\\
 // ------------ PUBLIC FUNCIONS ------------ \\\
 // ----------------------------------------- \\\
--- src/js/pages/buzz.js
+++ src/js/pages/buzz.js
@@ -17,108 +17,108 @@ var $filter         = $('.filter');
 // ----------------------------------------- \\\
 function init(){
 
-    var elem        = document.querySelector('.press-list');
-    var _dataAjax   = $filter.data('ajax');
-    var msnry       = null;
-     var _toggleClick    = false;;
+    // var elem        = document.querySelector('.press-list');
+    // var _dataAjax   = $filter.data('ajax');
+    // var msnry       = null;
+    //  var _toggleClick    = false;;
 
-    if($(window).width() > 550){
+    // if($(window).width() > 550){
 
-        setTimeout(function(){
-            msnry = new Masonry( elem, {
-                itemSelector: '.item',
-            });
+    //     setTimeout(function(){
+    //         msnry = new Masonry( elem, {
+    //             itemSelector: '.item',
+    //         });
 
-            msnry.layout();
+    //         msnry.layout();
 
-            msnry.on( 'layoutComplete', function(){
-                window.dispatchEvent(new Event('resize'));
-                console.log('update resize');
-            } );
-        }, 500 );
-    }
+    //         msnry.on( 'layoutComplete', function(){
+    //             window.dispatchEvent(new Event('resize'));
+    //             console.log('update resize');
+    //         } );
+    //     }, 500 );
+    // }
 
-    console.log('init 2');
-
-
-    $filter.find('.item').on('click', function(e){
-        e.preventDefault();
-
-        if(!_toggleClick){
-            $filter.find('.item').removeClass('active');
-            $(this).addClass('active');
-
-            var catID = $(this).data('id');
-            var limit = $(this).data('limit');
-
-            $('.press-list').addClass('loading');
-            $('.press-list').append('<div class="ajax-loader"><svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve"><path fill="#000" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"><animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform></path></svg></div>');
-            $('.press-list').find('.message').remove();
+    // console.log('init 2');
 
 
-            setTimeout(function(){
-                msnry.destroy();
-                // msnry = null;
-            },1000);
+    // $filter.find('.item').on('click', function(e){
+    //     e.preventDefault();
+
+    //     if(!_toggleClick){
+    //         $filter.find('.item').removeClass('active');
+    //         $(this).addClass('active');
+
+    //         var catID = $(this).data('id');
+    //         var limit = $(this).data('limit');
+
+    //         $('.press-list').addClass('loading');
+    //         $('.press-list').append('<div class="ajax-loader"><svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve"><path fill="#000" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"><animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite"></animateTransform></path></svg></div>');
+    //         $('.press-list').find('.message').remove();
+
+
+    //         setTimeout(function(){
+    //             msnry.destroy();
+    //             // msnry = null;
+    //         },1000);
             
 
-            $.ajax({
-                url: _dataAjax,
-                type: 'POST',
-                data: {
-                    action: 'kcg_filter_buzz_by_category',
-                    nonce: object_kcg.nonce,
-                    'cat-id': catID,
-                },
+    //         $.ajax({
+    //             url: _dataAjax,
+    //             type: 'POST',
+    //             data: {
+    //                 action: 'kcg_filter_buzz_by_category',
+    //                 nonce: object_kcg.nonce,
+    //                 'cat-id': catID,
+    //             },
 
-            }).done(function(response) {
+    //         }).done(function(response) {
 
-                var _html = response['html'];
+    //             var _html = response['html'];
             
-                setTimeout(function(){
+    //             setTimeout(function(){
 
-                    $('.press-list').find('.item').remove();
+    //                 $('.press-list').find('.item').remove();
 
-                    if(_html == ''){
-                        $('.press-list').append('<div class="message">Sorry, we don\'t have any item yet.</div>');
-                    }else{
-                        $('.press-list').append(_html);
-                    }
+    //                 if(_html == ''){
+    //                     $('.press-list').append('<div class="message">Sorry, we don\'t have any item yet.</div>');
+    //                 }else{
+    //                     $('.press-list').append(_html);
+    //                 }
 
-                    setTimeout(function(){
-                        if($(window).width() > 550){
-                            msnry = new Masonry( elem, {
-                                itemSelector: '.item',
-                            });
+    //                 setTimeout(function(){
+    //                     if($(window).width() > 550){
+    //                         msnry = new Masonry( elem, {
+    //                             itemSelector: '.item',
+    //                         });
 
-                            msnry.layout();
+    //                         msnry.layout();
 
-                            msnry.on( 'layoutComplete', function(){
-                                window.dispatchEvent(new Event('resize'));
-                                console.log('update resize');
-                            } );
-                        }
-                    }, 1000);
+    //                         msnry.on( 'layoutComplete', function(){
+    //                             window.dispatchEvent(new Event('resize'));
+    //                             console.log('update resize');
+    //                         } );
+    //                     }
+    //                 }, 1000);
 
-                    setTimeout(function(){
-                        // window.dispatchEvent(new Event('resize'));
-                        $('.press-list').find('.ajax-loader').remove();
-                        $('.press-list').removeClass('loading');
+    //                 setTimeout(function(){
+    //                     // window.dispatchEvent(new Event('resize'));
+    //                     $('.press-list').find('.ajax-loader').remove();
+    //                     $('.press-list').removeClass('loading');
                         
-                        _toggleClick = false;
-                    }, 1000);
+    //                     _toggleClick = false;
+    //                 }, 1000);
 
-                },1000);
+    //             },1000);
 
-                // console.log('ajax response '+ response);
+    //             // console.log('ajax response '+ response);
 
-            }).fail(function(response) {
-                console.log(response);
-            });
+    //         }).fail(function(response) {
+    //             console.log(response);
+    //         });
 
-            _toggleClick = true;
-        }
-    });
+    //         _toggleClick = true;
+    //     }
+    // });
     
 }
 
